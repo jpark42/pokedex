759cf8dc0a03de3d7043827e5d93bb236169ecc8
Johnny Park <110794898+jpark42@users.noreply.github.com>
2022-09-15 19:41:47 -0400

    Add pokemonList variable
    - added pokemonList variable array
    - included 3 objects in the array
    - included 3 keys for each object
    - included an array inside of the object for the "types" key

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-alert("Hello World");
-
-let favoriteFood = "ramen";
-document.write (favoriteFood);
-
+let pokemonList = [
+    {name: 'Charmelion', height: 1.1, types: ['fire']},
+    {name: 'Blaziken', height: 1.9, types: ['fire', 'fighting']},
+    {name: 'Aron', height: 0.4, types: ['rock', 'steel']}
+]
