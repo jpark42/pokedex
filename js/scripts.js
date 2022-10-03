52e6b8f59ede2cbcc708f2bd0624e8c96d9585e0
Johnny Park <110794898+jpark42@users.noreply.github.com>
2022-10-03 19:20:10 -0400

    DOM Manipulation
    - edited index page to include an unordered list
    - created an li element in javascript
    - create a button element in javascript
    - add a class to the button in javascript that has css tied to this class
    - append the button to the list item as its child and append the list item to the unordered list as it child
    - added addListItem function to the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -16,10 +16,28 @@ let pokemonRepository = (function() {
         pokemonList.push(item);
     }
 
+    function addListItem(pokemon){
+        //create a variable for pokemonList and selected the class we defined in html page using the querySelector function
+        let pokemonList = document.querySelector(".pokemon-list");
+        //create an li element using createElement function
+        let listPokemon = document.createElement("li");
+        //create a button using the createElement function
+        let button = document.createElement("button");
+        //gave the button inner text of pokemon.name
+        button.innerText = pokemon.name;
+        //gave the button we created a class
+        button.classList.add("button-class");
+        //append the button to the <li>
+        listPokemon.appendChild(button);
+        //append the <li> into the <ul>
+        pokemonList.appendChild(button);
+    }
+
     //return object as new public function assigned as keys
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        addListItem: addListItem
     }
 
 }) ()    
@@ -32,12 +50,16 @@ pokemonRepository.add({name: 'Chikorita', height: 35, types: ['leaf']});
 
 //creating a forEach function to reiterate over each pokemon in pokemonList
 pokemonRepository.getAll().forEach(function(pokemon) {
-    //if height of pokemon is greater than 1.9, then it will display 'Wow, that's big!' after it lists the pokemon
+    //forEach function will the run the loop over addListItem for the pokemonList array
+    pokemonRepository.addListItem(pokemon)
+
+    /*if height of pokemon is greater than 1.9, then it will display 'Wow, that's big!' after it lists the pokemon
     if (pokemon.height >= heightBig) {
         document.write(`<li>${pokemon.name}, (height: ${pokemon.height} inches), - Wow, that's big! Type(s):${pokemon.types}</li>`);
     } else {
     //for all other pokemon that are not greater than 1.9, it will normally list the pokemon as a list
         document.write(`<li>${pokemon.name}, (height: ${pokemon.height} inches), Type(s):${pokemon.types}</li>`);
     }
+    */
 });
 
