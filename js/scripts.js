1133107a3288da96f095b8779169736a2c618e62
Johnny Park <110794898+jpark42@users.noreply.github.com>
2022-10-09 14:30:02 -0400

    Fetching API
    - created a variable for an API url and created a loadList and loadDetails function that fetches info from the API
    - included promises and .then JSON responses

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
 let pokemonRepository = (function() {
     //defining a list of pokemon in an array
-    let pokemonList = [
-        {name: 'Charmelion', height: 43, types: ['fire']},
-        {name: 'Blaziken', height: 75, types: ['fire', 'fighting']},
-        {name: 'Aron', height: 16, types: ['rock', 'steel']}
-    ];
+    let pokemonList = [];
+    //API Url for pokedex with a pagination limit of 150 pokemon
+    let apiUrl='https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+
+    //define seperate function addPokemon()
+    function add(pokemon) {
+        if (
+          typeof pokemon === "object" &&
+          "name" in pokemon
+        ) {
+          pokemonList.push(pokemon);
+        } else {
+          console.log("pokemon is not correct");
+        }
+    }
     //define seperate function getAll()
     function getAll () {
         return pokemonList;
-    }
-
-    //define seperate function addItem()
-    function add(item) {
-        pokemonList.push(item);
     }
 
     function addListItem(pokemon){
@@ -38,38 +43,62 @@ let pokemonRepository = (function() {
         })
     }
 
-    //define a separate function showDetails() that writes the pokemon obect to the console
-    function showDetails(pokemon) {
-        console.log(pokemon);
+    //defining function loadList to fetch a list from the pokedex API
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+                console.log(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+    //function to load details of the pokemon by fetching them from the pokedex API
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+        //added details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+      //define a separate function showDetails() that uses a callback function to load details of pokemon and object to the console
+      function showDetails(item) {
+        loadDetails(item).then(function () {
+            console.log(item);
+        });
     }
 
     //return object as new public function assigned as keys
     return {
         getAll: getAll,
         add: add,
-        addListItem: addListItem
-    }
-
-}) ()    
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
+    };
+}) ();    
 
 //defining height threshhold for what is considered big
 let heightBig = 72;
 
-//adding a pokemon to pokemonRepository by using add function
-pokemonRepository.add({name: 'Chikorita', height: 35, types: ['leaf']});
-
-//creating a forEach function to reiterate over each pokemon in pokemonList
-pokemonRepository.getAll().forEach(function(pokemon) {
+pokemonRepository.loadList().then(function () {
+    //creating a forEach function to reiterate over each pokemon in pokemonList
+    pokemonRepository.getAll().forEach(function (pokemon) {
     //forEach function will the run the loop over addListItem for the pokemonList array
-    pokemonRepository.addListItem(pokemon)
-
-    /*if height of pokemon is greater than 1.9, then it will display 'Wow, that's big!' after it lists the pokemon
-    if (pokemon.height >= heightBig) {
-        document.write(`<li>${pokemon.name}, (height: ${pokemon.height} inches), - Wow, that's big! Type(s):${pokemon.types}</li>`);
-    } else {
-    //for all other pokemon that are not greater than 1.9, it will normally list the pokemon as a list
-        document.write(`<li>${pokemon.name}, (height: ${pokemon.height} inches), Type(s):${pokemon.types}</li>`);
-    }
-    */
+        pokemonRepository.addListItem(pokemon);
+    });
 });
-
